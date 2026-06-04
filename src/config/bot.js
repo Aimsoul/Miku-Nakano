74cdda7d12090124371dfa1e30e2eddfabe55387
Aimsoul <skillzhub3123@gmail.com>
2026-06-04 17:25:41 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "my other sisters",
+        name: "The Quintessential Quintuplets",
         // Activity type number (0 = Playing).
-        type: 5, 
+        type: 3, 
       },
     ],
   },
